4a268970b193121037474a6911e00603a9cbd503
Saurabh Sahu <147732631+saurabhrsahu@users.noreply.github.com>
2026-04-26 00:51:07 +0530

    Aks (#2)
     AKS module

--- Azure/AKS/provider.tf
+++ Azure/AKS/provider.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0.0, < 5.0.0"
+      version = ">= 3.110.0, < 4.0.0"
     }
     time = {
       source  = "hashicorp/time"
